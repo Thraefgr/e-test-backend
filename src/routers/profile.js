219d76772934ea0268af3b2f3a99b0e8f2000059
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2024-06-17 20:42:46 +0300

    Add a delete profile functionality to profile router

--- src/routers/profile.js
+++ src/routers/profile.js
@@ -9,7 +9,20 @@ router.get("/profile", checkAuth, async (req, res) => {
     const token = req.headers.authorization.split(" ")[1];
     const decoded = jwt.decode(token, process.env.SECRET_KEY)
     const userProfile = await User.findOne({username:decoded.username}, {_id:0, password:0, __v:0})
-    res.json(userProfile)
+    if(!userProfile) {
+        res.statusCode = 400;
+        res.json({message:"This user doesn't exist."});
+    } else {
+        res.json(userProfile)
+    }
+
+})
+
+router.delete("/profile", checkAuth, async (req, res) => {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.decode(token, process.env.SECRET_KEY);
+    const userProfile = await User.findOneAndDelete({username:decoded.username});
+    res.json({message:"Your profile is gone.Forever..."});
 })
 
 
