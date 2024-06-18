89a59ba42208b4ccb09fd82b52cb6f905a344231
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2024-06-18 12:46:30 +0300

    Add /tests/:id route so users can request individual file

--- src/routers/tests.js
+++ src/routers/tests.js
@@ -6,10 +6,25 @@ import Buffer from "node:buffer"
 const router = express.Router()
 
 router.get("/tests", checkAuth, async (req, res) => {
-    let allTests = await Test.find({}, {_id:0, __v:0, questions:0});
+    let allTests = await Test.find({}, {__v:0, questions:0});
     res.json(allTests)
 })
 
+router.get("/tests/:id", checkAuth, async (req, res) => {
+    try {
+        const id = req.params.id;
+        const test = await Test.findOne({_id:id}, {__v:0, questions:0});
+        if (!test) {
+            res.statusCode = 400;
+            res.json({message: "Enter a valid id! Or else!!"});
+        } else {
+            res.json(test)
+        }
+    } catch (error) {
+        res.statusCode = 400;
+        res.json({message: "Enter a valid id! Or else!!"})
+    }
+})
 
 const testsRouter = router;
 export default testsRouter;
