5bdca5d02debac9d40adb77c6299a3138ba56cee
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2024-06-21 12:12:44 +0300

    Fix checkAuth module, so it doesnt crash after the first request

--- src/middlewares/checkAuth.js
+++ src/middlewares/checkAuth.js
@@ -1,15 +1,16 @@
 import jwt from "jsonwebtoken";
 
 async function checkAuth(req, res, next) {
-    const authorization = req.headers.authorization
-    if(!authorization) {
-        res.statusCode = 400;
-        res.send({message: "You are not authorized motherfucker!"})
-    }
     try {
-        const token = authorization.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.SECRET_KEY);
-        next()
+        const authorization = req.headers.authorization
+        if(!authorization) {
+            res.statusCode = 400;
+            res.send({message: "You are not authorized motherfucker!"})
+        } else {
+            const token = authorization.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.SECRET_KEY);
+            next()
+        }
     } catch (error) {
         res.statusCode = 400;
         res.send({message:`${error}`})
