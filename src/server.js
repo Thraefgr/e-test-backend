5d85cc1521ced403a45b0a8c7dc396c63508c98e
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2024-06-21 14:10:21 +0300

    Allow requests from all origins

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import connectToDb from "./utils/connectToDb.js";
 import apiRouter from "./routers/router.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(apiRouter.signup);
 app.use(apiRouter.signin);
